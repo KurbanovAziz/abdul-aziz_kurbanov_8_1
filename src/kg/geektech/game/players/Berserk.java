a976b1dec19461dfae58f1a9037e2024166bbd29
KurbanovAziz <112629123+KurbanovAziz@users.noreply.github.com>
2022-09-24 15:09:14 +0600

    Initial commit

---
+++ src/kg/geektech/game/players/Berserk.java
@@ -0,0 +1,30 @@
+package kg.geektech.game.players;
+
+import kg.geektech.game.general.RPG_Game;
+
+import java.util.Random;
+
+public class Berserk extends Hero {
+    private int savedDamage;
+
+    public Berserk(int health, int damage, String name) {
+        super(health, damage, name, SuperAbility.SAVE_DAMAGE_AND_REVERT);
+    }
+
+    public int getSavedDamage() {
+        return savedDamage;
+    }
+
+    public void setSavedDamage(int savedDamage) {
+        this.savedDamage = savedDamage;
+    }
+
+    @Override
+    public void applySuperPower(Boss boss, Hero[] heroes) {
+        int damage = RPG_Game.random.nextInt(boss.getDamage());
+        setSavedDamage(this.getDamage() + damage);
+        boss.setHealth(boss.getHealth() - getSavedDamage());
+        System.out.println("Berserk saved damage: " +getSavedDamage());
+
+    }
+}
